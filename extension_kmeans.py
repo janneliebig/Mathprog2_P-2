e5020b661c33c9dc510334b8c62e522a5aabd46c
Janne Liebig <21lija1bif@hft-stuttgart.de>
2024-07-16 16:32:17 +0200

    Erweiterung

---
+++ extension_kmeans.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+class Kmeans:
+    def __init__(self, n_clusters=3, max_iterations=300, epsilon=1e-4):
+        self.n_clusters = n_clusters
+        self.max_iterations = max_iterations
+        self.epsilon = epsilon
+
+    def fit(self, X):
+
+        # Zufällige Wahl der n_clusters Mittelpunkte
+        self.centers = X[np.random.choice(X.shape[0], self.n_clusters, replace=False)]
+
+        for i in range(self.max_iterations):
+            # Labels setzen
+            self.labels = self.assign_labels(X)
+            # Neue Mittelpunkte bestimmen
+            new_centers = self.compute_centers(X)
+
+            # epsilon 
+            if np.all(np.linalg.norm(new_centers - self.centers, axis=1) < self.epsilon):
+                break
+            self.centers = new_centers
+
+    def assign_labels(self, X):
+        # eukl. Norm für die Abstände
+        distances = np.linalg.norm(X[:, np.newaxis] - self.centers, axis=2)
+        # kleinsten Abstand zurückgeben
+        return np.argmin(distances, axis=1)
+
+    def compute_centers(self, X):
+        # Leeres Array für Mittelpunkte (t+1)
+        centers = np.zeros((self.n_clusters, X.shape[1]))
+
+        # Mittelwert der Summe der Vektoren
+        for k in range(self.n_clusters):
+            centers[k] = X[self.labels == k].mean(axis=0)
+            
+        return centers
+
+    def predict(self, X):
+        # Clusterlabel für jeden Datenpunkt
+        return self.assign_labels(X)
+
+# Beispiel-Datensätze erstellen
+X, y = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
+
+# k-Means Algorithmus anwenden
+kmeans = Kmeans(n_clusters=4)
+kmeans.fit(X)
+y = kmeans.predict(X)
